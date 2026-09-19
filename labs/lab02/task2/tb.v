fcc8bd409d8a40fbf5141cdd31616e767f298016
Rishit-BITS <f20240951@pilani.bits-pilani.ac.in>
2026-09-19 15:43:13 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,11 +1,21 @@
 // tb.v
-// Starter testbench template -- YOU complete this file.
+// Testbench for parameterized lookup table.
 
 module tb;
 
-  // TODO: declare the inputs and outputs
+  reg  [2:0] t_sel;
+  wire [7:0] t_dout;
 
-  // TODO: instantiate DUT here
+  integer i;
+  integer errors;
+
+  lut #(
+    .WIDTH(8),
+    .DEPTH(8)
+  ) DUT (
+    .sel(t_sel),
+    .dout(t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -17,11 +27,34 @@ module tb;
   end
 
   initial begin
-    // TODO: apply different input combinations
+
+    errors = 0;
+
+    for (i = 0; i < 8; i = i + 1) begin
+
+      t_sel = i;
+      #5;
+
+      if (t_dout !== i * i) begin
+        $display("FAIL: sel=%d dout=%d expected=%d",
+                 t_sel, t_dout, i * i);
+
+        errors = errors + 1;
+      end
+      else begin
+        $display("PASS: sel=%d dout=%d",
+                 t_sel, t_dout);
+      end
+
+    end
+
+    $display("Total errors = %d", errors);
+
+    $finish;
 
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%b | dout=%d", t_sel, t_dout);
 
 endmodule
